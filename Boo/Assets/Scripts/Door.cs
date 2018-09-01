e8e04b8dab7c05d09a591bf75811be86c3582483
Andrew Velez <andrewmauro199617@yahoo.com>
2018-09-01 14:45:21 -0700

    Initial Commit

---
+++ Boo/Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Door : Interactable {
+
+    private Animator animator;
+
+    public override bool Interact()
+    {
+        if(!animator.GetBool("DoorOpened"))
+        {
+            animator.SetBool("DoorOpened", true);
+            return true;
+        }
+        return false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
